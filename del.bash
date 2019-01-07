f916d111d2f362cebf2c1abb7a9549e04780e988
miyamoto-yosuke <34563847+miyamoto-yosuke@users.noreply.github.com>
2019-01-07 15:37:36 +0900

    update del.bash

--- del.bash
+++ del.bash
@@ -1,5 +1,6 @@
 rm -rf node_modules yarn.lock
 docker-compose stop
 docker-compose rm
+docker container stop `docker container ls -aq`
 docker container rm `docker container ls -aq`
 docker image rm --force `docker images -q`
